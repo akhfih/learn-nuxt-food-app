8a35f4aae79ddabbf4b4669150b71219a95862ee
akhfih <akhfih.dev@gmail.com>
2023-06-17 16:33:59 +0700

    initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,10 @@
+<!-- Please remove this file from your project -->
+<template>
+  <main class="container"></main>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container">
+    <AppRestaurantInfo />
+  </main>
+</template>
+
+<script>
+import AppHeader from "../components/AppHeader.vue";
+import AppRestaurantInfo from "../components/AppRestaurantInfo.vue";
+
+export default {
+  name: "IndexPage",
+  components: { AppHeader, AppRestaurantInfo },
+};
+</script>
