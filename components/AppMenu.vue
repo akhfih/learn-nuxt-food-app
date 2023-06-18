317f777507333ee42b48ce772fe3c98997b2aec0
akhfih <akhfih.dev@gmail.com>
2023-06-18 16:05:48 +0700

    feat: use vuex store mutations and uuid

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -5,6 +5,7 @@
       <li><nuxt-link exact to="/restaurants">Restaurants</nuxt-link></li>
       <li><nuxt-link exact to="/about">About</nuxt-link></li>
     </ul>
+    <nuxt-link exact to="/cart">Cart</nuxt-link>
   </nav>
 </template>
 
--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -12,7 +12,9 @@
 
       <div class="quantity">
         <input type="number" min="1" v-model="count" />
-        <button class="primary">Add to Cart - ${{ combinedPrice }}</button>
+        <button @click="addToCart" class="primary">
+          Add to Cart - ${{ combinedPrice }}
+        </button>
       </div>
 
       <fieldset v-if="currentItem.options">
@@ -46,6 +48,11 @@
           <label :for="addon">{{ addon }}</label>
         </div>
       </fieldset>
+      <app-toast v-if="cartSubmitted">
+        Order Added!
+        <br />Return to
+        <nuxt-link to="/restaurants">restaurants</nuxt-link>
+      </app-toast>
     </section>
 
     <section class="options">
@@ -66,6 +73,7 @@ export default {
       itemOptions: "",
       itemAddons: [],
       itemSizeAndCost: [],
+      cartSubmitted: false,
     };
   },
   computed: {
@@ -88,6 +96,20 @@ export default {
     combinedPrice() {
       let total = this.count * this.currentItem.price;
       return total.toFixed(2);
+    },
+  },
+  methods: {
+    addToCart() {
+      let formOutput = {
+        item: this.currentItem.item,
+        count: this.count,
+        options: this.itemOptions,
+        addOns: this.itemAddons,
+        combinedPrice: this.combinedPrice,
+      };
+
+      this.cartSubmitted = true;
+      this.$store.commit("addToCart", formOutput);
     },
   },
 };
